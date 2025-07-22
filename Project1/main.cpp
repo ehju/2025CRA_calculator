d9eabcfdeb8103282b01a3c02640c063d9e353a9
seuni.park <seuni.park@samsung.com>
2025-07-22 15:18:02 +0900

    [New] Introduce getDivide feature 이 패치는 getDivide 함수를 구현한 부분 입니다. 특이점은 divide by zero상황시 exception발생토록 했습니다.
    Signed-off-by: seuni.park <seuni.park@samsung.com>

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,15 @@
 #include "gmock/gmock.h"
+#include <exception>
 class Cal {
 public:
 	// 이곳에 코드 작성
+	int getDivide(int a, int b)
+	{
+		if (b == 0)
+			throw std::exception("EINVAL - divide by zero\n");
+		else
+			return a / b;
+	}
 };
 // 테스트케이스 작성
 TEST(t1, t2) {
