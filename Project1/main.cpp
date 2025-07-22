c98dc431903e15c6a072463960caf32589bd09f9
sh1.lee <sh1.lee@samsung.com>
2025-07-22 15:16:17 +0900

    [feat] add MINUS Class

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
 #include "gmock/gmock.h"
 
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
+class MINUS {
+public:
+	int minus(int a, int b) {
+		return a - b;
+	}
+};
+
